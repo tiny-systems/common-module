515a40f86013cc9e8be3b7e88763ed4e8d8ac315
Maksym Trofimenko <maksym@container-registry.com>
2024-09-25 20:43:31 +0100

    refactor ticker

--- components/ticker/ticker.go
+++ components/ticker/ticker.go
@@ -5,97 +5,126 @@ import (
 	"fmt"
 	"github.com/tiny-systems/module/module"
 	"github.com/tiny-systems/module/registry"
-	"sync/atomic"
 	"time"
 )
 
 const (
 	ComponentName        = "ticker"
 	OutPort       string = "out"
-	StatusPort    string = "status"
 )
 
 type Context any
 
-type Status struct {
-	Status string `json:"status" readonly:"true" title:"Status" colSpan:"col-span-6"`
-	Reset  bool   `json:"reset" format:"button" title:"Reset" required:"true" colSpan:"col-span-6"`
-}
-
 type Settings struct {
-	Context          Context `json:"context,omitempty" configurable:"true" title:"Context" description:"Arbitrary message to be send each period of time"`
-	Period           int     `json:"period" required:"true" title:"Periodicity (ms)" minimum:"10" default:"1000"`
-	EnableStatusPort bool    `json:"enableStatusPort" required:"true" title:"Enable status port" description:"Status port"`
+	Context Context `json:"context,omitempty" configurable:"true" title:"Context" description:"Arbitrary message to be send each period of time"`
+	Delay   int     `json:"delay" required:"true" title:"Delay (ms)" description:"Delay between signals" minimum:"0" default:"1000"`
+	Auto    bool    `json:"auto" title:"Auto send" required:"true" description:"Start sending as soon as component deployed"`
 }
 
 type Component struct {
-	counter  int64
 	settings Settings
+	cancel   context.CancelFunc
+	ctx      context.Context
 }
 
 func (t *Component) Instance() module.Component {
 	return &Component{
 		settings: Settings{
-			Period: 1000,
+			Delay: 1000,
 		},
 	}
 }
 
 type Control struct {
-	Start bool `json:"start" required:"true" title:"Component state"`
+	Context Context `json:"context" required:"true" title:"Context"`
+	Start   bool    `json:"start" format:"button" colSpan:"col-span-6" title:"Start" required:"true"`
+	Stop    bool    `json:"stop" format:"button" colSpan:"col-span-6" title:"Stop" required:"true"`
 }
 
 func (t *Component) GetInfo() module.ComponentInfo {
 	return module.ComponentInfo{
 		Name:        ComponentName,
 		Description: "Ticker",
-		Info:        "Sends messages periodically",
+		Info:        "Sends messages periodically with defined delay. Next message being sent as soon as port unblocked",
 		Tags:        []string{"SDK"},
 	}
 }
 
 // Emit non a pointer receiver copies Component with copy of settings
 func (t *Component) emit(ctx context.Context, handler module.Handler) error {
-	ticker := time.NewTicker(time.Duration(t.settings.Period) * time.Millisecond)
-	defer ticker.Stop()
-	for {
-		select {
-		case <-ticker.C:
 
-			atomic.AddInt64(&t.counter, 1)
+	for {
+		timer := time.NewTimer(time.Duration(t.settings.Delay) * time.Millisecond)
+		select {
+		case <-timer.C:
 			_ = handler(ctx, OutPort, t.settings.Context)
+
 		case <-ctx.Done():
+			timer.Stop()
 			return ctx.Err()
 		}
 	}
 }
 
 func (t *Component) Handle(ctx context.Context, handler module.Handler, port string, msg interface{}) error {
-	if port == module.SettingsPort {
-		settings, ok := msg.(Settings)
+
+	switch port {
+	case module.ControlPort:
+		in, ok := msg.(Control)
+		if !ok {
+			return fmt.Errorf("invalid input msg")
+		}
+
+		t.settings.Context = in.Context
+
+		if in.Stop && t.cancel != nil {
+			// we run and asked to stop
+			t.cancel()
+			t.cancel = nil
+			// redraw
+			_ = handler(ctx, module.ReconcilePort, nil)
+			return nil
+		}
+		if in.Start && t.cancel == nil {
+			//asked to start and not running
+			var ctx2 context.Context
+
+			ctx2, t.cancel = context.WithCancel(ctx)
+			// redraw
+			_ = handler(ctx, module.ReconcilePort, nil)
+			return t.emit(ctx2, handler)
+		}
+	case module.SettingsPort:
+		in, ok := msg.(Settings)
 		if !ok {
 			return fmt.Errorf("invalid settings")
 		}
-		if settings.Period < 10 {
-			return fmt.Errorf("period should be more than 10 milliseconds")
-		}
-		t.settings = settings
-		return nil
-	}
+		t.settings = in
 
-	return fmt.Errorf("invalid message")
+		if t.settings.Auto {
+			return t.emit(ctx, handler)
+		}
+	}
+	return nil
+}
+
+func (t *Component) getControl() Control {
+	c := Control{
+		Stop:    t.cancel == nil,
+		Start:   t.cancel != nil,
+		Context: t.settings.Context,
+	}
+	return c
 }
 
 func (t *Component) Ports() []module.Port {
+
 	ports := []module.Port{
 		{
-			Name:   module.SettingsPort,
-			Label:  "Settings",
-			Source: true,
-
-			Configuration: Settings{
-				Period: 1000,
-			},
+			Name:          module.SettingsPort,
+			Label:         "Settings",
+			Source:        true,
+			Configuration: t.settings,
 		},
 		{
 			Name:          OutPort,
@@ -104,19 +133,13 @@ func (t *Component) Ports() []module.Port {
 			Position:      module.Right,
 			Configuration: new(Context),
 		},
+		{
+			Name:          module.ControlPort,
+			Label:         "Control",
+			Configuration: t.getControl(),
+		},
 	}
 
-	if t.settings.EnableStatusPort {
-		ports = append(ports, module.Port{
-			Name:     StatusPort,
-			Label:    "Status",
-			Source:   true,
-			Position: module.Bottom,
-			Configuration: Status{
-				Status: fmt.Sprintf("All good: %d", t.counter),
-			},
-		})
-	}
 	return ports
 }
 
