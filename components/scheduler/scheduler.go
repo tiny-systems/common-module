4f3f29dbe0b9b3fb25c7c1cde3bf8250a73759f4
Tiny Systems <dev@tinysystems.io>
2024-11-14 18:16:01 +0000

    update ticker and scheduler

--- components/scheduler/scheduler.go
+++ components/scheduler/scheduler.go
@@ -6,6 +6,8 @@ import (
 	cmap "github.com/orcaman/concurrent-map/v2"
 	"github.com/tiny-systems/module/module"
 	"github.com/tiny-systems/module/registry"
+	"go.opentelemetry.io/otel/trace"
+	"sync"
 	"time"
 )
 
@@ -15,10 +17,25 @@ const (
 	InPort        string = "in"
 	StartPort     string = "start"
 	AckPort       string = "ack"
+	StopPort      string = "stop"
 )
 
 type Settings struct {
-	EnableAckPort bool `json:"enableAckPort" title:"Enable task acknowledge port" description:"Port gives information if incoming task was scheduled properly"`
+	EnableAckPort  bool `json:"enableAckPort" title:"Enable task acknowledge port" description:"Port gives information if incoming task was scheduled properly"`
+	EnableStopPort bool `json:"enableStopPort" required:"true" title:"Enable stop port" description:"Stop port allows you to stop scheduler"`
+}
+
+type StartControl struct {
+	Start  bool   `json:"start" format:"button" title:"Start" required:"true" description:"Run"`
+	Status string `json:"status" title:"Status" readonly:"true"`
+}
+
+type StopControl struct {
+	Stop   bool   `json:"stop" format:"button" title:"Stop" required:"true" description:"Stop"`
+	Status string `json:"status" title:"Status" readonly:"true"`
+}
+
+type Stop struct {
 }
 
 type Start struct {
@@ -51,18 +68,26 @@ type TaskAck struct {
 
 type task struct {
 	timer *time.Timer
-	call  func()
+	call  func(ctx context.Context)
 	id    string
 }
 
 type Component struct {
-	settings Settings
-	tasks    cmap.ConcurrentMap[string, *task]
+	settings       Settings
+	cancelFunc     context.CancelFunc
+	cancelFuncLock *sync.Mutex
+
+	//
+	runCtx  context.Context
+	runLock *sync.Mutex
+	tasks   cmap.ConcurrentMap[string, *task]
 }
 
 func (s *Component) Instance() module.Component {
 	return &Component{
-		tasks: cmap.New[*task](),
+		cancelFuncLock: &sync.Mutex{},
+		runLock:        &sync.Mutex{},
+		tasks:          cmap.New[*task](),
 	}
 }
 
@@ -75,96 +100,176 @@ func (s *Component) GetInfo() module.ComponentInfo {
 	}
 }
 
-func (s *Component) emit(ctx context.Context) error {
-	for _, k := range s.tasks.Keys() {
-		v, _ := s.tasks.Get(k)
-		go s.waitTask(ctx, v)
+func (s *Component) run(ctx context.Context, handler module.Handler) error {
+
+	s.runLock.Lock()
+	defer s.runLock.Unlock()
+
+	runCtx, runCancel := context.WithCancel(ctx)
+	defer runCancel()
+
+	s.runCtx = runCtx
+
+	s.setCancelFunc(runCancel)
+	// reconcile so show we are listening
+	_ = handler(context.Background(), module.ReconcilePort, nil)
+
+	defer func() {
+		s.setCancelFunc(nil)
+		_ = handler(context.Background(), module.ReconcilePort, nil)
+	}()
+
+	<-s.runCtx.Done()
+	return nil
+}
+
+func (s *Component) setCancelFunc(f func()) {
+	s.cancelFuncLock.Lock()
+	defer s.cancelFuncLock.Unlock()
+	s.cancelFunc = f
+}
+
+func (s *Component) isRunning() bool {
+	s.cancelFuncLock.Lock()
+	defer s.cancelFuncLock.Unlock()
+	return s.cancelFunc != nil
+}
+
+func (s *Component) stop() error {
+	s.cancelFuncLock.Lock()
+	defer s.cancelFuncLock.Unlock()
+	if s.cancelFunc == nil {
+		return nil
 	}
-	<-ctx.Done()
+	s.cancelFunc()
 	return nil
 }
 
 func (s *Component) Handle(ctx context.Context, handler module.Handler, port string, msg interface{}) error {
 
-	//emit
-	if port == module.SettingsPort {
+	switch port {
+	case module.SettingsPort:
 		in, ok := msg.(Settings)
 		if !ok {
 			return fmt.Errorf("invalid settings")
 		}
 		s.settings = in
 		return nil
-	}
 
-	if port == StartPort {
-		return s.emit(ctx)
-	}
+	case module.ControlPort:
+		if msg == nil {
+			break
+		}
+		switch msg.(type) {
+		case StartControl:
+			return s.run(ctx, handler)
+		case StopControl:
+			return s.stop()
+		}
 
-	if port != InPort {
+	case StartPort:
+		return s.run(ctx, handler)
+
+	case StopPort:
+		return s.stop()
+
+	case InPort:
+		in, ok := msg.(InMessage)
+		if !ok {
+			return fmt.Errorf("invalid input task message")
+		}
+		var (
+			t           = in.Task
+			scheduledIn int64
+		)
+
+		if in.Task.Schedule {
+			scheduledIn = int64(t.DateTime.Sub(time.Now()).Seconds())
+		}
+
+		ackErr := s.addOrUpdateTask(t.ID, t.Schedule, t.DateTime.Sub(time.Now()), func(ctx context.Context) {
+			_ = handler(ctx, OutPort, OutMessage{
+				Task:    in.Task,
+				Context: in.Context,
+			})
+		})
+
+		if s.settings.EnableAckPort {
+			ack := TaskAck{
+				Task:        in.Task,
+				Context:     in.Context,
+				ScheduledIn: scheduledIn,
+			}
+			if ackErr != nil {
+				ackErrStr := ackErr.Error()
+				ack.Error = &ackErrStr
+			}
+
+			if err := handler(ctx, AckPort, ack); err != nil {
+				return err
+			}
+		} else if ackErr != nil {
+			return ackErr
+		}
+
+		//
+
+	default:
 		return fmt.Errorf("invalid port: %s", port)
 	}
-
-	in, ok := msg.(InMessage)
-	if !ok {
-		return fmt.Errorf("invalid message")
-	}
-
-	var (
-		t           = in.Task
-		scheduledIn int64
-	)
-	if in.Task.Schedule {
-		scheduledIn = int64(t.DateTime.Sub(time.Now()).Seconds())
-	}
-
-	if s.settings.EnableAckPort {
-		if err := handler(ctx, AckPort, TaskAck{
-			Task:        in.Task,
-			Context:     in.Context,
-			ScheduledIn: scheduledIn,
-		}); err != nil {
-			return err
-		}
-	}
-
-	s.addOrUpdateTask(t.ID, t.Schedule, t.DateTime.Sub(time.Now()), func() {
-		_ = handler(ctx, OutPort, OutMessage{
-			Task:    in.Task,
-			Context: in.Context,
-		})
-	})
 	return nil
 }
 
-func (s *Component) addOrUpdateTask(id string, start bool, duration time.Duration, f func()) {
+func (s *Component) addOrUpdateTask(id string, schedule bool, duration time.Duration, f func(ctx context.Context)) error {
+
+	if !s.isRunning() {
+		return fmt.Errorf("scheduler is not running")
+	}
+	if duration.Seconds() < 0 {
+		return fmt.Errorf("scheduled time is past")
+	}
+
 	if d, ok := s.tasks.Get(id); ok {
-		// job is registered
-		// tasks it
+		// stop and remove it
 		d.timer.Stop()
-		if start {
-			d.timer.Reset(duration)
-		} else {
-			s.tasks.Remove(id)
-		}
-		return
+		s.tasks.Remove(id)
 	}
-	if !start {
-		return
+	// not found and don't ask to schedule
+	if !schedule {
+		return nil
 	}
+
+	// schedule a new task
 	tt := &task{
 		timer: time.NewTimer(duration),
 		id:    id,
 		call:  f,
 	}
+
 	s.tasks.Set(id, tt)
+	go s.waitTask(tt)
+	return nil
 }
 
-func (s *Component) waitTask(ctx context.Context, d *task) {
+func (s *Component) waitTask(d *task) {
+
+	defer s.tasks.Remove(d.id)
 	select {
 	case <-d.timer.C:
-		s.tasks.Remove(d.id)
-		d.call()
-	case <-ctx.Done():
+		// new trace
+		d.call(trace.ContextWithSpanContext(s.runCtx, trace.NewSpanContext(trace.SpanContextConfig{})))
+	case <-s.runCtx.Done():
+	}
+}
+
+func (s *Component) getControl() interface{} {
+	if s.isRunning() {
+		return StopControl{
+			Status: "Running",
+		}
+	}
+	return StartControl{
+		Status: "Not running",
 	}
 }
 
@@ -184,12 +289,17 @@ func (s *Component) Ports() []module.Port {
 			Position:      module.Left,
 		},
 		{
+			Name:          module.ControlPort,
+			Label:         "Dashboard",
+			Configuration: s.getControl(),
+		},
+		{
 			Name:   InPort,
 			Label:  "Tasks",
 			Source: true,
 			Configuration: InMessage{
 				Task: Task{
-					ID:       "someID2323",
+					ID:       "someUniqueID",
 					DateTime: time.Now(),
 					Schedule: true,
 				},
@@ -205,6 +315,17 @@ func (s *Component) Ports() []module.Port {
 		},
 	}
 
+	// programmatically stop server
+	if s.settings.EnableStopPort {
+		ports = append(ports, module.Port{
+			Position:      module.Bottom,
+			Name:          StopPort,
+			Label:         "Stop",
+			Source:        true,
+			Configuration: Stop{},
+		})
+	}
+
 	if !s.settings.EnableAckPort {
 		return ports
 	}
@@ -214,7 +335,7 @@ func (s *Component) Ports() []module.Port {
 		Label:         "Ack",
 		Source:        false,
 		Configuration: TaskAck{},
-		Position:      module.Bottom,
+		Position:      module.Right,
 	})
 }
 
